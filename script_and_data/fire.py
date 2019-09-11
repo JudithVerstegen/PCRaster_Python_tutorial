25f25f9a8d3dd591748162d99ff358e5b1732740
Judith Verstegen <judithverstegen@gmail.com>
2019-09-11 10:32:09 +0200

    changed indentation, as some students were experiencing problems with their editors

--- script_and_data/fire.py
+++ script_and_data/fire.py
@@ -2,15 +2,15 @@ from pcraster import *
 from pcraster.framework import *
 
 class Fire(DynamicModel):
-	def __init__(self):
-		DynamicModel.__init__(self)
-		setclone('clone.map')
-
-	def initial(self):
-		pass
-
-	def dynamic(self):
-		pass
+  def __init__(self):
+    DynamicModel.__init__(self)
+    setclone('clone.map')
+    
+  def initial(self):
+    pass
+  
+  def dynamic(self):
+    pass
 
 nrOfTimeSteps=100
 myModel = Fire()
